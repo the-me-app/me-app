afd2676dc182dd01e89183ce68ca83744444dad2
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2024-06-25 09:22:27 +0500

    created basic folder structure and added home page

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+import Home from '~screens/home/Home';
+
+function HomePage({}) {
+  return <Home />;
+}
+
+export default HomePage;
---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,11 @@
+function Home({}) {
+  return (
+    <div style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <span style={{ color: '#000', fontSize: 25, fontWeight: 'bold' }}>
+        Home
+      </span>
+    </div>
+  );
+}
+
+export default Home;
